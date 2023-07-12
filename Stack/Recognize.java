844412cb3961fdb43e33e5ee122e72caf5d53ee1
Viral1127 <viralchauhan4438@gmail.com>
2023-07-12 21:02:24 +0530

    DS

--- Stack/Recognize.java
+++ Stack/Recognize.java
@@ -1,107 +1,109 @@
-import java.util.*;
-class Stack{
-	Scanner sc = new Scanner(System.in);
-	int top;
-	char[] stack;
-	int maxsize;
+public class Recognize{
+	public static void main(String args[]){
+		String v = "c_";
+		int i=0;
+		char c = v.charAt(i);
+		StackOperation s1 = new StackOperation(v.length());
+		//push
+		while (c != 'c' && c != '_'){
+			s1.push(c);
+			i++;
+			c = v.charAt(i);
+		}
+		if (c != '_' ){
+			i++;
+			c = v.charAt(i);
+		}
 
-	Stack(int size){
-		maxsize = size;
-		stack = new	char[maxsize];
-		top=0;
+		//pop
+		while (c != '_' && ! (s1.isEmpty()) ){
+			if(c == s1.peep()){
+				s1.pop();
+			}
+			else{
+				break;
+			}
+			i++;
+			c = v.charAt(i);
+		}
+		if(c == '_' && s1.isEmpty()){
+			System.out.println("Valid .. ");
+		}
+		else {
+			System.out.println("InValid . ");
+		}
+	}
+}
+
+
+
+class StackOperation{
+	int top;
+	char[] Stack;
+	int n;
+	StackOperation(int n){
+		this.n = n;
+		Stack = new char[n];
+		top=-1;
 	}
 	
 	void push(char element){
-		if(top==maxsize-1){
+
+		//System.out.println("enter push element :  ");
+		//int x = sc.nextInt();
+		if(top>=n){
 			System.out.println("overflow");
 			return;
 		}
 		else{
 			top++;
-			stack[top] = element;
+			Stack[top] = element;
 			//System.out.println(value);
 		}
 	}
 
-	public char pop(){
-		char v;
-		if(top == 0){
+	int pop(){
+		int v = 0;
+		if(top == -1){
 			System.out.println("stack underflow");
 			return 0;
 		}
 		else{
+			v = Stack[top];
 			top--;
-			v = stack[top];
 			return v;
 			//System.out.println();
 		}
 	}
 
-	public char peep(){
-		if(top==0){
-			System.out.println("stack is empty");
-			return 0;
+	char peep(){
+		if(top==-1){
+			return ' ';	
 		}
 		else{
-			return stack[top];
+			return Stack[top];
 		}
 	}
 
 	void display(){
-		if(top==0){
+		if(top==-1){
 			System.out.println("Stack is empty :  ");
 		}
 		else{
 		for(int i=top ; i>0 ; i--){
-			System.out.println(stack[i]);
+			System.out.println(Stack[i]);
 			}
+		}
+		
+	}
+
+	boolean isEmpty(){
+		if (top == -1){
+			return true;
+		}
+		else{
+			return false;
 		}
 	}
 
-	public boolean isEmpty(){
-            return(top == 0);
-        }	
 }
-
-public class Recognize{
-	public static void main(String[] args){
-		String[] testStrings = {"aca", "bcb", "abcba", "abbcbba"};
-
-            
-        for (String str : testStrings){
-        	Stack s1 = new Stack(str.length( ));
-            boolean isRecognized = false;
-
-        	for (int i = 0; i < str.length(); i++) {
-                char currentChar = str.charAt(i);
-
-                if (currentChar == 'a') {
-                    s1.push(currentChar);
-                } else if (currentChar == 'b') {
-                    if (s1.isEmpty() || s1.peep() != 'a') {
-                        isRecognized = true;
-                        break;
-                    } else {
-                        s1.pop();
-                    }
-                } else if (currentChar == 'c') {
-                    if (s1.isEmpty() || s1.peep() != 'b') {
-                        isRecognized = true;
-                        break;
-                    } else {
-                        s1.pop();
-                    }
-                } else {
-                    isRecognized = true;
-                    break;
-                }
-            }
-
-            if (isRecognized && s1.isEmpty()) {
-                System.out.println("String \"" + str + "\" is recognized.");
-            } else {
-                System.out.println("String \"" + str + "\" is not recognized.");
-            }
-        } 
-        }
-    }
