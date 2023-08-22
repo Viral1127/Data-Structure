31dcdca4dc370c7bbf2cd35f544c3395dafc9899
Viral1127 <viralchauhan4438@gmail.com>
2023-08-22 23:15:25 +0530

    DS

--- Tree/Tree.java
+++ Tree/Tree.java
@@ -48,6 +48,42 @@ class Bts{
 		}
 	}
 
+	    void delete(int value) {
+        root = deleteNode(root, value);
+    }
+
+    Node deleteNode(Node root, int value) {
+        if (root == null) {
+            return root;
+        }
+
+        if (value < root.value) {
+            root.left = deleteNode(root.left, value);
+        } else if (value > root.value) {
+            root.right = deleteNode(root.right, value);
+        } else {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            root.value = Successor(root.right);
+            root.right = deleteNode(root.right, root.value);
+        }
+
+        return root;
+    }
+
+    int Successor(Node root) {
+        int Successor = root.value;
+        while (root.left != null) {
+            Successor = root.left.value;
+            root = root.left;
+        }
+        return Successor;
+    }
+
 	static void inOrder(Node root){
 		if(root==null){
 			return;
@@ -83,6 +119,14 @@ class Bts{
 			System.out.println(root.value + " ");
 		}
 	}
+
+	// void treeStructure(Node root, String prefix, boolean isLeft) {
+    //     if (root != null) {
+    //         System.out.println(prefix + (isLeft ? "|--" : " |- ") + root.value);
+    //         treeStructure(root.left, prefix + (isLeft ? "│   " : "    "), true);
+    //         treeStructure(root.right, prefix + (isLeft ? "│   " : "    "), false);
+    //     }
+    // }
 }
 
 public class Tree{
@@ -92,12 +136,13 @@ public class Tree{
 		Scanner sc = new Scanner(System.in);
 
         while(true){
-            System.out.println("enter choice :  \n 1 for insert\n 2 for  Preorder Traversal\n 3  Post order Traversal\n 4 in order Traversal\n 5 for Exit ");
+            System.out.println("enter choice :  \n 1 for insert\n 2 for  Preorder Traversal\n 3  Post order Traversal\n 4 in order Traversal\n 5 for delete\n 6 for Exit ");
             int choice = sc.nextInt();
             switch (choice){
                 case 1 : System.out.println("Enter element");
                             int x = sc.nextInt();
                             b1.insert(x);
+                            // b1.treeStructure(b1.root, "", false);
                             break;
                 case 2 : b1.preOrder(b1.root);
                 			break;
@@ -108,7 +153,12 @@ public class Tree{
                 case 4 : b1.inOrder(b1.root);
                 			break;
 
-                case 5 : return;			
+                case 5 : System.out.println("Enter element which u want to delete");
+                            int y = sc.nextInt();
+                            b1.delete(y);
+                            break;
+
+                case 6 : return;			
           	}
         }
 	}
